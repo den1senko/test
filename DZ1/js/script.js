6343361a1dfb4feb1f08228a667f5455cd2a18ec
Sergey <densir.1919@gmail.com>
2019-02-10 15:42:47 +0200

    Это наш первый комит

---
+++ DZ1/js/script.js
@@ -0,0 +1,30 @@
+var money = prompt("Ваш бюджет на месяц?"),
+    time = prompt("Введите дату в формате YYYY-MM-DD");
+    
+var firstAnswer1 = prompt("Введите обязательную статью расходов в этом месяце"),
+    secondAnswer1 = prompt("Во сколько обойдется?"),
+    firstAnswer2 = prompt("Введите обязательную статью расходов в этом месяце"),
+    secondAnswer2 = prompt("Во сколько обойдется?");
+
+var appData = {
+    budget: money,
+    timeData: time,
+    expenses: {
+        firstAnswer1: secondAnswer1,
+        firstAnswer2: secondAnswer2
+    },
+    optionalExpenses: {
+
+    },
+    income: [],
+    savings: false
+};
+
+alert(appData.budget/30);
+
+console.log(appData.budget);
+console.log(appData.timeData);
+console.log(appData.expenses);
+console.log(appData.optionalExpenses);
+console.log(appData.income);
+console.log(appData.savings);
